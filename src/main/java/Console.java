44adc594961d2043535f1c11071e974bac7cd269
Victor Rudkovsky <vpcomtek@gmail.com>
2021-10-25 15:44:01 +0300

    Update Console.java

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -1,5 +1,5 @@
 public class Console {
     public static void main(String[] args) {
-        System.out.println("Flight Booking Application");
+        System.out.println("Flight Booking Application Java");
     }
 }
